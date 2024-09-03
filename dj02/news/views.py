34d89d3ce6228d5eb31e68f64459769ae87174a5
max6971 <cmytt@mail.ru>
2024-09-03 23:33:57 +0700

    Create views.py

---
+++ dj02/news/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .models import News_post
+from .forms import News_postForm
+# Create your views here.
+def home(request):
+
+    news = News_post.objects.all()
+    return render(request,'news/news.html', {'news': news})
+
+def create_news(request):
+    error = ""
+    if request.method == 'POST':
+        form = News_postForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news')
+        else:
+            error = "Проверьте записи"
+
+    form = News_postForm()
+    return render(request,'news/add_news.html',{'form' : form, 'error': error})
